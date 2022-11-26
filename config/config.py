6ed368fcc5c153aff5db98b453a1cdc628d0aad2
Hexor'xD <hexorxd@teamesportxd.tech>
2022-11-26 09:12:23 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -28,7 +28,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 # Custom max audio(music) duration for voice chat. set DURATION_LIMIT in variables with your own time(mins), Default to 60 mins.
 DURATION_LIMIT_MIN = int(
-    getenv("DURATION_LIMIT", "200")
+    getenv("DURATION_LIMIT", "150")
 )  # Remember to give value in Minutes
 
 # Duration Limit for downloading Songs in MP3 or MP4 format from bot
@@ -115,7 +115,7 @@ PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", "25"))
 
 # Cleanmode time after which bot will delete its old messages from chats
 CLEANMODE_DELETE_MINS = int(
-    getenv("CLEANMODE_MINS", "300")
+    getenv("CLEANMODE_MINS", "10")
 )  # Remember to give value in Seconds
 
 
